c9686d7b498b5d4e39f9ff425c7d037f5238b26d
Aravind-Fullstack Developer <aravind.silambam@gmail.com>
2024-07-23 12:10:37 +0530

    logo update

--- backend/app.js
+++ backend/app.js
@@ -25,12 +25,12 @@ app.use('/api/v1/',payment);
 
 app.use('/api/v1/',fileRouter_S);
 
-// if(process.env.NODE_ENV === "production") {
-//     app.use(express.static(path.join(__dirname, '../frontend/build')));
-//     app.get('*', (req, res) =>{
-//         res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
-//     })
-// }
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+    })
+}
 
 app.use(errorMiddleware)
 
